f302160c06cd3528ebdcc94f17e2aa6590125d63
munyokii <munyoki912@gmail.com>
2025-07-15 15:27:15 +0300

    :sparkles: Added ckeditor configurations and path

--- estore/urls.py
+++ estore/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("ckeditor5/", include('django_ckeditor_5.urls')),
     path("__reload__/", include("django_browser_reload.urls")),
     path("", include("macrotech.urls")),
     path("accounts/", include("custom_user.urls")),
