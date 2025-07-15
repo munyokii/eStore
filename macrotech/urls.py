d50a2e4810ac33d937f1eb371e39a7e738a867ad
munyokii <munyoki912@gmail.com>
2025-07-15 13:42:36 +0300

    :sparkles: Created function for sending contact message

--- macrotech/urls.py
+++ macrotech/urls.py
@@ -4,14 +4,15 @@ from . import views
 
 urlpatterns = [
   path("", views.HomeView.as_view(), name="home"),
-  path("product/details/<int:product_id>/", views.ProductDetailView.as_view(), name="product_detail"),
+  path("product/details/<int:product_id>/",
+       views.ProductDetailView.as_view(), name="product_detail"),
   path("cart/", views.CartView.as_view(), name="cart"),
   path("checkout/", views.CheckoutView.as_view(), name="checkout"),
   path("blog/", views.BlogView.as_view(), name="blog"),
   path("blog/details/", views.BlogDetailView.as_view(), name="blog_detail"),
   path("category/", views.CategoryView.as_view(), name="category"),
   path("about/", views.AboutView.as_view(), name="about"),
-  path("contact/", views.ContactView.as_view(), name="contact"),
+  path("macrotech/contact-us/", views.ContactView.as_view(), name="contact"),
   path("support/", views.SupportView.as_view(), name="support"),
   path("shipping-info/", views.ShippingInfoView.as_view(), name="shipping_info"),
   path("return-policy/", views.ReturnPolicyView.as_view(), name="return_policy"),
