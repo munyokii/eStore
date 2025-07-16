5232c6788481a334f11c82ed9b4e5ec708e3e7b5
munyokii <munyoki912@gmail.com>
2025-07-16 20:37:03 +0300

    :sparkles: Added category model to admin

--- macrotech/admin.py
+++ macrotech/admin.py
@@ -1,8 +1,9 @@
 """Application admin configuration for the Macrotech app."""
 from django.contrib import admin
 
-from macrotech.models import Product, Review
+from macrotech.models import Category, Product, Review
 
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Review)
+admin.site.register(Category)
