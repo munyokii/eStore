a417b407f2b833cb62083ba2dd1ee0e475f2d66c
munyokii <munyoki912@gmail.com>
2025-07-17 11:12:23 +0300

    :sparkles: Updated logging configurations

--- estore/settings.py
+++ estore/settings.py
@@ -279,3 +279,17 @@ CKEDITOR_5_CONFIGS = {
         }
     }
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
