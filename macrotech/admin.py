ec0a084b9383d7b5a59b1d89a0697c3d441ebe35
munyokii <munyoki912@gmail.com>
2025-07-17 11:14:11 +0300

    :sparkles: Created function to enable admins send email to newsletter subscribers

--- macrotech/admin.py
+++ macrotech/admin.py
@@ -1,9 +1,62 @@
 """Application admin configuration for the Macrotech app."""
+import logging
+import smtplib
+
 from django.contrib import admin
+from django import forms
+from django.core.mail import send_mail
+from django.conf import settings
+from django_ckeditor_5.fields import CKEditor5Widget
 
-from macrotech.models import Category, Product, Review
+from macrotech.models import Category, EmailTemplate, NewsletterSubscriber, Product, Review
 
+
+logger = logging.getLogger(__name__)
 # Register your models here.
+class EmailTemplateAdminForm(forms.ModelForm):
+    """Email Template Admin"""
+    class Meta:
+        """Class meta"""
+        model = EmailTemplate
+        fields = '__all__'
+        widgets = {
+            'message': CKEditor5Widget(),
+        }
+
+class EmailTemplateAdmin(admin.ModelAdmin):
+    """Admin template"""
+    form = EmailTemplateAdminForm
+
+    def save_related(self, request, form, formsets, change):
+        """Save M2M and then send email"""
+        super().save_related(request, form, formsets, change)
+
+        obj = form.instance
+        subject = obj.subject
+        html_message = obj.message
+
+        recipients = [subscriber.email for subscriber in obj.recipients.all()]
+        from_email = settings.EMAIL_HOST_USER
+
+        if recipients:
+            try:
+                send_mail(
+                    subject,
+                    "",
+                    from_email,
+                    recipients,
+                    html_message=html_message,
+                    fail_silently=False
+                )
+                logger.warning("Email sent successfully to: %s", recipients)
+            except smtplib.SMTPException as e:
+                logger.exception(
+                    "Failed to send email to: %s. SMTP Error: %s", recipients, str(e))
+        else:
+            logger.warning("No recipients found. Email not sent.")
+
 admin.site.register(Product)
 admin.site.register(Review)
 admin.site.register(Category)
+admin.site.register(NewsletterSubscriber)
+admin.site.register(EmailTemplate,  EmailTemplateAdmin)
