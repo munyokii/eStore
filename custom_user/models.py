a48d997e9b150ed5dcbb638131669cce6ee1a3b8
munyokii <munyoki912@gmail.com>
2025-07-18 21:31:56 +0300

    :sparkles: Added avatar field

--- custom_user/models.py
+++ custom_user/models.py
@@ -27,6 +27,11 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    avatar = models.ImageField(
+        upload_to='avatars/',
+        blank=True,
+        null=True,
+    )
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
